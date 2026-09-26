cc9403f25526316d86c94b02b0c30700f5f004e7
victor <quesvictor4235@gmail.com>
2026-09-26 14:35:25 +0200

    style: format Projects.js with prettier

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -48,17 +48,17 @@ export default function Projects() {
         <div className="main" id="opensource">
           <h1 className="project-title">Open Source Projects</h1>
           <div className="repo-cards-div-main">
-                      {repo.map((v, i) => {
-                        if (!v || !v.node) {
-                          console.error(
-                            `Github Object for repository number : ${i} is undefined or private`
-                          );
-                          return null;
-                        }
-                        return (
-                          <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
-                        );
-                      })}
+            {repo.map((v, i) => {
+              if (!v || !v.node) {
+                console.error(
+                  `Github Object for repository number : ${i} is undefined or private`
+                );
+                return null;
+              }
+              return (
+                <GithubRepoCard repo={v} key={v.node.id} isDark={isDark} />
+              );
+            })}
           </div>
           <Button
             text={"More Projects"}
